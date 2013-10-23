9194a7fa1d6acfe50a0c05fc5a178e0c06b6ad31
Matthew Kirkley <matt.kirkley@gmail.com>
2013-10-23 17:06:42 -0400

    new compiled jar..

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-INPUT_FOLDER=/home/matt/src/school/ajax/assignment1
+INPUT_FOLDER=/home/matt/src/school/ajax/assignment4/src
 OUTPUT_FILENAME=out.pdf
 java -cp build/libs/ajax_class_pdfgenerator.jar edu.kirkley.util.AjaxClassPDFGenerator -i $INPUT_FOLDER -o $OUTPUT_FILENAME
